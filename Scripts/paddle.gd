c82449f54b163291e0a5ca52a343ff0e1f545d43
GeorgiePoo <121971574+GeorgiePoo@users.noreply.github.com>
2023-11-14 11:51:38 -0800

    finally finished #5

--- Scripts/paddle.gd
+++ Scripts/paddle.gd
@@ -12,12 +12,12 @@ var is_ball_started = false
 
 
 @onready var ball = $"../Ball" as Ball
-@onready var collision_shape_2D = $CollisionShape2D
+@onready var collision_shape_2d = $CollisionShape2D
 
 func _ready():
 	ball.life_lost.connect(on_ball_lost)
 	camera_rect = camera.get_viewport_rect()
-	half_paddle_width = collision_shape_2D.shape.get_rect().size.x / 2 * scale.x
+	half_paddle_width = collision_shape_2d.shape.get_rect().size.x / 2 * scale.x
 
 func _physics_process(delta):
 	linear_velocity = speed * direction
@@ -45,3 +45,6 @@ func _input(event):
 func on_ball_lost():
 	is_ball_started = false
 	direction = Vector2.ZERO
+	
+func get_width():
+	return collision_shape_2d.shape.get_rect().size.x
